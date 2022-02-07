00f97cf393606eca57550cee2055da6bf7972c52
liviazeviani <liviapavinizeviani@gmail.com>
2022-02-07 15:39:45 -0300

    Imprimindo apenas os elementos do vetor, sem a impressão nos null, utilizando StringBuilder

--- src/Vetor.java
+++ src/Vetor.java
@@ -38,6 +38,23 @@ public class Vetor {
 
     @Override
     public String toString() {
-        return Arrays.toString(elementos);
+
+        //utilizando uma classe própria do Java: StringBuilder
+
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for (int i =0; i<this.tamanho-1; i++){
+            s.append(this.elementos[i]);
+            s.append(",");
+        }
+
+        if (this.tamanho>0){
+            s.append(this.elementos[this.tamanho-1]);
+        }
+
+        s.append("]");
+
+        return s.toString();
     }
 }
